5b7a65155f223e9c101534e73f720345d97e7c24
KataKappel <kappelkatalin@gmail.com>
2023-09-22 18:38:50 +0200

    for loop

--- script.js
+++ script.js
@@ -1,96 +1,24 @@
-let stringVariable = "text"
-let numberVariable = 15
-let booleanVariable = true
+console.log ('loaded')
+let rootElement = document.querySelector('#root')
+//console.log(rootElement)
 
-function myFunc(param1, param2) {
-    return param1 + param2
+//rootElement.insertAdjacentHTML('beforeend', '<h2>Hello world</h2>')
+
+let htmlContent = "<h2>Hello world</h2>";
+rootElement.insertAdjacentHTML('beforeend', htmlContent)
+
+/* let counter = 0;
+while (counter < 10) {
+    rootElement.insertAdjacentHTML('beforeend', htmlContent)
+    counter++;
+} */
+
+let array ["alma", "körte", "szilva", "eper", "birs"]
+
+for (let counter = 1; counter <= 10; counter++) {
+    console.log(counter) 
+    rootElement.insertAdjacentHTML('beforeend', htmlContent)
 }
-
-console.log(stringVariable)
-console.log("stringVariable")
-console.log(stringVariable, numberVariable, booleanVariable)
-
-let array = ["alma", 12, false, undefined, null]
-console.log(array)
-
-let stringArray = ["Kata", "Zsombor", "Imola"]
-console.log(stringArray[0])
-
-let myObj = {
-    key1: "value1",
-    key2: "value2",
-    key3: "value3",
-    key4: 15,
-    key5: 600,
-    key6: true,
-    key7: ["Kata", "Zsombor", "Imola"]
+for (let index = 0; index <= 10; index++) {
+    rootElement.insertAdjacentHTML('beforeend', `<h2> ${array[index]} </h2>`);
 }
-
-console.log(myObj.key7[2])
-
-let myFacebook = {
-    name: "Kata",
-    age: 28,
-    location: "Budapest",
-    friends: ["Gipsz Jakab", "John Doe", "Példa Péter"],
-    favoriteMovies: [
-        {
-            title: "Harry Potter",
-            year: 2012,
-            characters: [
-                "Harry Potter",
-                "Ronald Weasley",
-                "Hedvig",
-            ]
-        },
-        {
-            title: "Üvegtigris",
-            year: 2004,
-            characters: [
-                "Csoki",
-                "Gaben",
-                "Lali",
-            ]
-        }
-    ]
-}
-
-console.log(myFacebook.favoriteMovies[0].characters[1])
-
-function negativeOrPositive(number) {
-    if (number < 0) {
-        return "negative"
-    } else if (number > 0){
-        return "positive"
-    } else {
-        return "zero"
-    }
-}
-
-function logger(text) {
-    console.log(text)
-}
-
-/* logger(negativeOrPositive(0)) */
-
-function decideIfBeerIsGood(beer) {
-    if (beer.price < 340 && beer.abv > 4.7) {
-        return beer.name + ": jó sör, mert olcsó és üt"
-    } else if (beer.price < 340) {
-        return beer.name + ": jó sör, mert olcsó"
-    } else if (beer.abv > 4.7) {
-        return beer.name + ": jó sör, mert üt"
-    } else {
-        return beer.name + ": nem jó sör"
-    }
-}
-
-let dreher = { name: "Dreher", price: 349, abv: 5 }
-let soproni = { name: "Soproni", price: 329, abv: 4.5 }
-let borsodi = { name: "Borsodi", price: 339, abv: 4.8 }
-let pecsi = {name: "Pécsi", price: 379, abv: 4.2 }
-
-logger(decideIfBeerIsGood(dreher)) 
-logger(decideIfBeerIsGood(soproni)) 
-logger(decideIfBeerIsGood(borsodi))
-logger(decideIfBeerIsGood(pecsi)) 
